9d220ffc70174edabcb07a4dcc589dae026e9067
Inhyeok Kim <haruband@gmail.com>
2018-08-29 10:41:59 +0900

    Add debug helper codes

---
+++ driver/dbmisc.c
@@ -0,0 +1,46 @@
+#include <linux/string.h>
+
+void __raonfs_message(const char *function, unsigned int line, const char *tag, const char *fmt, ...)
+{
+	struct va_format vaf;
+	va_list args;
+
+	va_start(args, fmt);
+
+	vaf.fmt = fmt;
+	vaf.va = &args;
+
+	printk(KERN_NOTICE "RAONFS: %s:%d: %s: %pV\n", function, line, tag, &vaf);
+
+	va_end(args);
+}
+
+void __raonfs_error(const char *function, unsigned int line, const char *tag, const char *fmt, ...)
+{
+	struct va_format vaf;
+	va_list args;
+
+	va_start(args, fmt);
+
+	vaf.fmt = fmt;
+	vaf.va = &args;
+
+	printk(KERN_CRIT "RAONFS error: %s:%d: comm %s: %s: %pV\n", function, line, current->comm, tag, &vaf);
+
+	va_end(args);
+}
+
+void __raonfs_warning(const char *function, unsigned int line, const char *tag, const char *fmt, ...)
+{
+	struct va_format vaf;
+	va_list args;
+
+	va_start(args, fmt);
+
+	vaf.fmt = fmt;
+	vaf.va = &args;
+
+	printk(KERN_WARNING "RAONFS warning: %s:%d: comm %s: %s: %pV\n", function, line, current->comm, tag, &vaf);
+
+	va_end(args);
+}
