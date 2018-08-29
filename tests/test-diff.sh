95e657e7a14041ca4f64036d71052cb42b7cbb31
Inhyeok Kim <haruband@gmail.com>
2018-08-29 13:42:30 +0900

    Add a test using diff to compare all contents

---
+++ tests/test-diff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+SOURCE=$1
+TARGET=$2
+MOUNTO=.mnt
+LDEV=$(losetup -f)
+
+MKRAONFS=../tools/mkraonfs.py
+
+$MKRAONFS -s $SOURCE -t $TARGET
+mkdir -p $MOUNTO
+losetup $LDEV $TARGET
+mount -t raonfs $LDEV $MOUNTO
+
+diff -r $SOURCE $MOUNTO
+
+umount $MOUNTO
+losetup -d $LDEV
+rm -rf $MOUNTO
+rm -f $TARGET
