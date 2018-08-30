f580f955e6898961c45593f3b852882c8eadd4aa
Inhyeok Kim <haruband@gmail.com>
2018-08-30 15:55:28 +0900

    Add raonfs_trace()

--- driver/dbmisc.h
+++ driver/dbmisc.h
@@ -1,6 +1,8 @@
 #ifndef __RAONFS_DBMISC_H__
 #define __RAONFS_DBMISC_H__
 
+#define raonfs_trace()	\
+	__raonfs_trace(__func__, __LINE__)
 #define raonfs_notice(fmt, ...)	\
 	__raonfs_notice(__func__, __LINE__, fmt, ##__VA_ARGS__)
 #define raonfs_error(fmt, ...)	\
@@ -8,6 +10,7 @@
 #define raonfs_warning(fmt, ...)	\
 	__raonfs_warning(__func__, __LINE__, fmt, ##__VA_ARGS__)
 
+extern void __raonfs_trace(const char *function, unsigned int line);
 extern void __raonfs_notice(const char *function, unsigned int line, const char *fmt, ...);
 extern void __raonfs_error(const char *function, unsigned int line, const char *fmt, ...);
 extern void __raonfs_warning(const char *function, unsigned int line, const char *fmt, ...);
