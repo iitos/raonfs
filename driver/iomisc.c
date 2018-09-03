a6192a1e740bdb0c3e904240e80f511de2666db7
Inhyeok Kim <haruband@gmail.com>
2018-09-03 11:29:47 +0900

    Remove unnecessary checks

--- driver/iomisc.c
+++ driver/iomisc.c
@@ -7,16 +7,10 @@
  */
 size_t raonfs_block_read(struct super_block *sb, unsigned long pos, void *buf, size_t len)
 {
-	struct raonfs_sb_info *sbi = RAONFS_SB(sb);
 	struct buffer_head *bh;
 	unsigned long offset;
 	size_t segment;
 	size_t remains;
-
-	if (pos >= sbi->fssize)
-		return -EIO;
-	if (len > sbi->fssize - pos)
-		len = sbi->fssize - pos;
 
 	for (remains = len; remains > 0; buf += segment, remains -= segment, pos += segment) {
 		offset = pos & (sb->s_blocksize - 1);
@@ -36,16 +30,10 @@ size_t raonfs_block_read(struct super_block *sb, unsigned long pos, void *buf, s
  */
 size_t raonfs_block_strcpy(struct super_block *sb, unsigned long pos, char *buf, size_t len)
 {
-	struct raonfs_sb_info *sbi = RAONFS_SB(sb);
 	struct buffer_head *bh;
 	unsigned long offset;
 	size_t segment;
 	size_t remains;
-
-	if (pos >= sbi->fssize)
-		return -EIO;
-	if (len > sbi->fssize - pos)
-		len = sbi->fssize - pos;
 
 	for (remains = len; remains > 0; buf += segment, remains -= segment, pos += segment) {
 		offset = pos & (sb->s_blocksize - 1);
@@ -67,17 +55,11 @@ size_t raonfs_block_strcpy(struct super_block *sb, unsigned long pos, char *buf,
  */
 ssize_t raonfs_block_strlen(struct super_block *sb, unsigned long pos, size_t limit)
 {
-	struct raonfs_sb_info *sbi = RAONFS_SB(sb);
 	struct buffer_head *bh;
 	unsigned long offset;
 	size_t segment;
 	ssize_t len = 0;
 	u_char *buf, *p;
-
-	if (pos >= sbi->fssize)
-		return -EIO;
-	if (limit > sbi->fssize - pos)
-		limit = sbi->fssize - pos;
 
 	for (; limit > 0; limit -= segment, pos += segment, len += segment) {
 		offset = pos & (sb->s_blocksize - 1);
@@ -100,17 +82,11 @@ ssize_t raonfs_block_strlen(struct super_block *sb, unsigned long pos, size_t li
  */
 int raonfs_block_strcmp(struct super_block *sb, unsigned long pos, const char *str, size_t size)
 {
-	struct raonfs_sb_info *sbi = RAONFS_SB(sb);
 	struct buffer_head *bh;
 	unsigned long offset;
 	size_t segment;
 	bool matched;
 	bool terminated = false;
-
-	if (pos >= sbi->fssize)
-		return -EIO;
-	if (size + 1 > sbi->fssize - pos)
-		return -EIO;
 
 	for (; size > 0; size -= segment, pos += segment, str += segment) {
 		offset = pos & (sb->s_blocksize - 1);
